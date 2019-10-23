b147086d6543dc165a97d172ad6ec46c6a14a04d
MH Rilwan <rilwanfit@gmail.com>
2019-10-23 16:59:55 +0200

    Revert "Simple console app with symfony 4 dependency injection support"
    This reverts commit d4b6c90a

--- var/cache/dev/ContainerGPaOtKh/removed-ids.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'App\\Command\\CreateUserCommand' => true,
-    'Psr\\Container\\ContainerInterface' => true,
-    'Symfony\\Component\\DependencyInjection\\ContainerInterface' => true,
-];
